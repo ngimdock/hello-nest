9525e94ce50e09b667b42d94ef5d741f2e3f1906
ngimdock <ngimdockzemfack@gmail.com>
2022-10-09 07:09:45 +0100

    amélioratioen du décorateur User de sorte qu'il prends des propriétés en paramèttre

--- src/decorators/user.decorator.ts
+++ src/decorators/user.decorator.ts
@@ -1,9 +1,10 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
 export const User = createParamDecorator(
-  (data: unknown, ctx: ExecutionContext) => {
+  (data: string, ctx: ExecutionContext) => {
     const request = ctx.switchToHttp().getRequest();
+    const user = request.user;
 
-    return request.user;
+    return data ? user?.[data] : user;
   },
 );
