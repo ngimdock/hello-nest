fa3301a78654f064eb15f178aa6a301b9a385e85
ngimdock <ngimdockzemfack@gmail.com>
2022-10-14 21:31:42 +0100

    setup a custom provider with the useVale propertie

--- src/cats/cats.module.ts
+++ src/cats/cats.module.ts
@@ -2,9 +2,35 @@ import { Module } from '@nestjs/common';
 import { CatsController } from './cats.controller';
 import { CatsService } from './cats.service';
 
+const mockCatsService = {
+  name: 'gabriel',
+
+  findAll: () => {
+    return 'All the cats';
+  },
+
+  create: () => {
+    return 'cat created';
+  },
+
+  remove: () => {
+    return 'cat removed';
+  },
+
+  findOne: () => {
+    return 'This is a single cat';
+  },
+};
+
 @Module({
   controllers: [CatsController],
-  providers: [CatsService],
+  providers: [
+    {
+      provide: CatsService,
+      // useClass: CatsService,
+      useValue: mockCatsService,
+    },
+  ],
   exports: [CatsService],
 })
 export class CatsModule {
